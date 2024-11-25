e525bdf0404ec71d182737753d34a602aaa65697
rodrigo <rodrigo.palacios.salas@ucsp.edu.pe>
2024-11-24 19:39:16 -0500

    cambios en A_punteros y Creacion de la clase economia

--- A_punteros.cpp
+++ A_punteros.cpp
@@ -1,6 +1,6 @@
 #include "A_punteros.hpp"
-#include <iostream>
 
+// Calcula la longitud de la cadena
 int A_puntero::c_Longitud(const char* ptm) 
 {
     int longitud = 0;
@@ -11,28 +11,40 @@ int A_puntero::c_Longitud(const char* ptm)
     return longitud;
 }
 
+// Copia la cadena origen en destino (memoria dinámica)
 void A_puntero::copiarCadena(char*& destino, const char* origen) 
 {
-    delete[] destino; // Liberar memoria no se bien si esta implementacion es correcta
-    destino = new char[c_Longitud(origen) + 1];
-    for (int i = 0; i < c_Longitud(origen); ++i) 
+    if (origen == nullptr) 
+    {
+        destino = nullptr;
+        return;
+    }
+
+    int longitud = c_Longitud(origen);
+
+    // Liberar la memoria anterior solo si destino no es nullptr
+    if (destino != nullptr) 
+    {
+        delete[] destino;
+    }
+
+    // Asignar nueva memoria para la cadena de destino
+    destino = new char[longitud + 1];  // +1 para el carácter nulo '\0'
+
+    // Copiar la cadena
+    for (int i = 0; i < longitud; ++i) 
     {
         destino[i] = origen[i];
     }
-    destino[c_Longitud(origen)] = '\0';
+    destino[longitud] = '\0'; // Agregar el carácter nulo al final
 }
 
+// Imprime la cadena
 void A_puntero::imprimirCadena(const char* cadena) 
 {
-    for (const char* ptr = cadena; *ptr != '\0'; ++ptr) 
-    {
-        std::cout << *ptr;
+    if (cadena == nullptr || cadena[0] == '\0') {
+        std::cout << "Cadena vacía" << std::endl;
+    } else {
+        std::cout << cadena << std::endl;
     }
-    std::cout << "\n";
-}
-
-
-A_puntero::A_puntero()
-{
-
 }
